f2716da800ee3ab1f71aa966ee2581fc5797c52a
dbotsford <3408480+dawsbot@users.noreply.github.com>
2014-07-19 03:36:34 -0400

    URL -> IP complete

--- ipmapper.py
+++ ipmapper.py
@@ -1,3 +1,17 @@
 """
   Take inputted URL and return lat/long
 """
+import sys
+import socket
+
+def main():
+  #User inputted one URL 
+  if len(sys.argv) != 2:
+       print "ERROR: please input 1 URL"
+       return
+ 
+  url = sys.argv[1]
+  print socket.gethostbyname(url)
+  
+  
+main()
